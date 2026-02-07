df27098ec03cb83f6a24482ee38ee5493b3cff3b
Sameer Agarwal <sameeragarwal007@outlook.com>
2026-02-07 00:59:18 -0600

    Auth fixes

--- networkplus/src/auth.ts
+++ networkplus/src/auth.ts
@@ -1,16 +1,13 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma"
 
+import authConfig from "./auth.config"
+
 export const { handlers, auth: nextAuthAuth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
 
 export const auth = async (...args: any[]) => {
